541c1cf69e4c71e6c77aff7529536bd1ccd88e25
SREE <SREE@DESKTOP-OHJSHRN>
2019-01-06 12:10:16 +0530

    restful client and server config

--- src/main/java/com/rest/services/IplService.java
+++ src/main/java/com/rest/services/IplService.java
@@ -1,13 +1,15 @@
 package com.rest.services;
 
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
-import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 import com.rest.pojo.Score;
+import com.rest.pojo.Team;
 
 @Path("/ipl")
 public class IplService {
@@ -30,6 +32,7 @@ public class IplService {
 	@GET
 	@Produces(MediaType.APPLICATION_JSON)
 	public Response getScore() {
+		System.out.println("Requested for score!!");
 		Score score = new Score();
 		score.setCountryName("India");
 		score.setRuns(340);
@@ -39,6 +42,18 @@ public class IplService {
 		score.setBat2("Jadeja");
 		
 		return Response.status(200).entity(score).build();
+	}
+	
+	@Path("/registerForIpl")
+	@POST
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response registerTeam(Team team) {
+		System.out.println(team.getTeamName());
+		
+		team.setStatus("Accepted");
+		//If team is accepted make a entry in db
+		return Response.status(201).entity(team).build();
 	}
 	
 }
